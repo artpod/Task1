c3aa1465670027bf0d1f43e15650eb31dead6f47
artpod <rabotnikovv@gmail.com>
2021-10-04 22:41:30 +0300

    Task1.1
    Task1.1

---
+++ Home1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Home1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
